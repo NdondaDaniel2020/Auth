b63da1c5bfe251ad45bf6fa228d6b25c500b82fe
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-08-05 02:19:15 +0100

    chore: add dependencia da db app/api/dependencies/database.py com get_db (dependência assíncrona que fornece uma AsyncSession por request e garante o fechamento/commit/rollback correto).

---
+++ app/api/dependencies/database.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.session import get_db
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_db)]
